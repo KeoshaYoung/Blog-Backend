6021a9d56c567e6f5b8f73ec7e3004ab30a7124e
keoshayoung <keoshayoung@email.com>
2022-08-11 14:42:24 -0400

    created show test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,4 +15,21 @@ RSpec.describe "Posts", type: :request do
       expect(posts.length).to eq(2)
     end
   end
+
+  describe "GET /posts/:id" do
+    it "should return a hash with all of the appropriate attributes" do
+      user = User.create!(name: "Test", email: "test@test.com", password: "password")
+
+      Post.create!(user_id: user.id, title: "Test Title1", body: "Test Body1", image: "Test Img1")
+
+      post_id = Post.first.id
+      get "/posts/#{post_id}.json"
+      post = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(post["title"]).to eq("Test Title1")
+      expect(post["body"]).to eq("Test Body1")
+      expect(post["image"]).to eq("Test Img1")
+    end
+  end
 end
